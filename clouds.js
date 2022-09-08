e911d61ed06f072ae48f9503eafdc028f324f453
Boyan Velikov <boyanvelikov@gmail.com>
2022-09-08 06:49:26 +0300

    Add files via upload

---
+++ clouds.js
@@ -0,0 +1,21 @@
+function jumpingOnClouds(c) {
+    let jumps = 0;
+    let currentIndex = 0;
+    let nextIndex1;
+    let nextIndex2;
+    
+    while (currentIndex < c.length-1) {
+        nextIndex1 = currentIndex + 1;
+        nextIndex2 = currentIndex + 2;
+        if (c[nextIndex2] == 0) {
+            currentIndex = nextIndex2;
+        } else {
+            currentIndex = nextIndex1;
+        }
+        jumps++;
+    }
+    
+    return jumps;
+}
+
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
